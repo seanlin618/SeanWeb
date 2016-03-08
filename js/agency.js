3eeefe727a19d3d76c8a397816bfc866fa9c4a71
Sean Lin <teli5023@colorado.edu>
2016-03-08 17:09:49 +0800

    test new video bg
    *Coder video bg

--- js/agency.js
+++ js/agency.js
@@ -26,18 +26,104 @@ $('.navbar-collapse ul li a').click(function() {
 });
 
 $('#btnfood').on('click',function(){
-	$('#portfolioModal1').modal('hide');
-	$('#videofood').remove();
-	$('.tv').remove();
+    $('#portfolioModal1').modal('hide');
+    $('#videofood').remove();
+    $('.tv').remove();
 })
 
 $('#closefood').on('click',function(){
-	$('#portfolioModal1').modal('hide');
-	$('#videofood').remove();
-	$('.tv').remove();
+    $('#portfolioModal1').modal('hide');
+    $('#videofood').remove();
+    $('.tv').remove();
 })
 $('#foodOpen').on('click',function(){
-	$('#portfolioModal1').modal('show');
-	$('#pvideo').append('<img class="tv" src="img/portfolio/tv.png" alt="">');
-	$('#pvideo').append('<iframe id="videofood" width="420" height="315" src="https://www.youtube.com/embed/q7e-hIru8qE" frameborder="0" allowfullscreen></iframe>');
+    $('#portfolioModal1').modal('show');
+    $('#pvideo').append('<img class="tv" src="img/portfolio/tv.png" alt="">');
+    $('#pvideo').append('<iframe id="videofood" width="420" height="315" src="https://www.youtube.com/embed/q7e-hIru8qE" frameborder="0" allowfullscreen></iframe>');
 })
+
+
+
+//jQuery is required to run this code
+$( document ).ready(function() {
+
+    scaleVideoContainer();
+
+    initBannerVideoSize('.video-container .poster img');
+    initBannerVideoSize('.video-container .filter');
+    initBannerVideoSize('.video-container video');
+
+    $(window).on('resize', function() {
+        scaleVideoContainer();
+        scaleBannerVideoSize('.video-container .poster img');
+        scaleBannerVideoSize('.video-container .filter');
+        scaleBannerVideoSize('.video-container video');
+    });
+
+});
+
+
+
+//jQuery is required to run this code
+$( document ).ready(function() {
+
+    scaleVideoContainer();
+
+    initBannerVideoSize('.video-container .poster img');
+    initBannerVideoSize('.video-container .filter');
+    initBannerVideoSize('.video-container video');
+
+    $(window).on('resize', function() {
+        scaleVideoContainer();
+        scaleBannerVideoSize('.video-container .poster img');
+        scaleBannerVideoSize('.video-container .filter');
+        scaleBannerVideoSize('.video-container video');
+    });
+
+});
+
+function scaleVideoContainer() {
+
+    var height = $(window).height() + 5;
+    var unitHeight = parseInt(height) + 'px';
+    $('.homepage-hero-module').css('height',unitHeight);
+
+}
+
+function initBannerVideoSize(element){
+
+    $(element).each(function(){
+        $(this).data('height', $(this).height());
+        $(this).data('width', $(this).width());
+    });
+
+    scaleBannerVideoSize(element);
+
+}
+
+function scaleBannerVideoSize(element){
+
+    var windowWidth = $(window).width(),
+    windowHeight = $(window).height() + 5,
+    videoWidth,
+    videoHeight;
+
+    console.log(windowHeight);
+
+    $(element).each(function(){
+        var videoAspectRatio = $(this).data('height')/$(this).data('width');
+
+        $(this).width(windowWidth);
+
+        if(windowWidth < 1000){
+            videoHeight = windowHeight;
+            videoWidth = videoHeight / videoAspectRatio;
+            $(this).css({'margin-top' : 0, 'margin-left' : -(videoWidth - windowWidth) / 2 + 'px'});
+
+            $(this).width(videoWidth).height(videoHeight);
+        }
+
+        $('.homepage-hero-module .video-container video').addClass('fadeIn animated');
+
+    });
+}
